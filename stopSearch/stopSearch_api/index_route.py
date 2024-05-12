ae71435054b564bf0a49d34c727d25a6e3be420b
PythonAkoto <yawakoto.python@example.com>
2024-05-12 19:13:23 +0100

    database models created

--- stopSearch/stopSearch_api/index_route.py
+++ stopSearch/stopSearch_api/index_route.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from stopSearch import app
-from random import randint
+from stopSearch.stopSearch_database.extension import init_db
+init_db()
 
 @app.route("/")
 def index_route() -> list[dict]:
--- stopSearch/stopSearch_database/extension.py
+++ stopSearch/stopSearch_database/extension.py
@@ -5,27 +5,42 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 load_dotenv()
 
-engine: create_engine = create_engine(
+engine = create_engine(
     os.environ["STOPSEARCH_DB"],
-    connect_args={'check_same_thread': False},
+    # connect_args={'check_same_thread': False},
+    pool_pre_ping=True  # Optional: enable pool_pre_ping to prevent MySQL server has gone away error
 )
-db_session: scoped_session = scoped_session(
+db_session = scoped_session(
     sessionmaker(
         autocommit=False,
         autoflush=False,
         bind=engine,
     )
 )
-LocalSession: sessionmaker = sessionmaker(
+LocalSession = sessionmaker(
     autocommit=False,
     autoflush=False,
     expire_on_commit=False,
     bind=engine
 )
+
 Base: declarative_base = declarative_base()
 Base.query = db_session.query_property()
 
 # initialise database
 def init_db():
-    import stopSearch.stopSearch_database.models
+    from stopSearch.stopSearch_database.models import Base
     Base.metadata.create_all(bind=engine)
+
+    # Create default data if it doesn't exist
+    from stopSearch.stopSearch_database.models import QuestionReportType
+    session = db_session()
+    if not session.query(QuestionReportType).first():
+        default_data = [
+            QuestionReportType(reportType='Type1'),
+            QuestionReportType(reportType='Type2'),
+            # Add more default data as needed
+        ]
+        session.add_all(default_data)
+        session.commit()
+        session.close()
