a0a345900dfd020cd2eecbe43cc69487965d8592
PythonAkoto <yawakoto.python@example.com>
2024-05-26 21:49:58 +0100

    Database models set up for questions & answers

--- stopSearch/stopSearch_database/models.py
+++ stopSearch/stopSearch_database/models.py
@@ -4,134 +4,132 @@ from stopSearch.stopSearch_database.extension import Base
 
 # questions
 class QuestionReportType(Base):
-    __tablename__ = 'questionReportType'
-    questionReportTypeID = Column(Integer, primary_key=True)
-    reportType = Column(String(8)) # witness / victim 
+    __tablename__ = 'question_report_type'
+    question_report_type_id = Column(Integer, primary_key=True)
+    report_type = Column(String(8)) # witness / victim 
 
 class QuestionLocationType(Base):
-    __tablename__ = 'questionLocationType'
-    questionLocationTypeID = Column(Integer, primary_key=True)
-    locationType = Column(String(20)) # automatic address / manual address
+    __tablename__ = 'question_location_type'
+    question_location_type_id = Column(Integer, primary_key=True)
+    location_type = Column(String(20)) # automatic address / manual address
 
 class QuestionVictimsInvolved(Base):
-    __tablename__ = 'questionVictimsInvolved'
-    questionVictimsInvolvedID = Column(Integer, primary_key=True)
-    victimsInvolved = Column(String(8))
+    __tablename__ = 'question_victims_involved'
+    question_victims_involved_id = Column(Integer, primary_key=True)
+    victims_involved = Column(String(8))
 
 class QuestionNumberOfPolice(Base):
-    __tablename__ = 'questionNumberOfPolice'
-    questionNumberOfPoliceID = Column(Integer, primary_key=True)
-    numberOfPolice = Column(String(8))
+    __tablename__ = 'question_number_of_police'
+    question_number_of_police_id = Column(Integer, primary_key=True)
+    number_of_police = Column(String(8))
 
 class QuestionSearchReason(Base):
-    __tablename__ = 'questionSearchReason'
-    questionsearchTypeID = Column(Integer, primary_key=True)
-    searchReason = Column(String(50))
+    __tablename__ = 'question_serarch_type'
+    question_serarch_type_id = Column(Integer, primary_key=True)
+    search_reason = Column(String(85))
 
 class QuestionSearchType(Base):
-    __tablename__ = 'questionSearchType'
-    questionSearchTypeID = Column(Integer, primary_key=True)
-    searchType = Column(String(10)) # moderate / aggressive
+    __tablename__ = 'question_search_type'
+    question_search_type_id = Column(Integer, primary_key=True)
+    search_type = Column(String(10)) # moderate / aggressive
 
 class QuestionVictimAge(Base):
-    __tablename__ = 'questionVictimAge'
-    questionVictimAgeID = Column(Integer, primary_key=True)
-    victimAge = Column(String(8))
+    __tablename__ = 'question_victim_age'
+    question_victim_age_id = Column(Integer, primary_key=True)
+    victim_age = Column(String(8))
 
 class QuestionVictimGender(Base):
-    __tablename__ = 'questionVictimGender'
-    questionVictimGenderID = Column(Integer, primary_key=True)
-    victimGender = Column(String(30))
+    __tablename__ = 'question_victim_gender'
+    question_victim_gender_id = Column(Integer, primary_key=True)
+    victim_gender = Column(String(30))
 
 class QuestionVictimRace(Base):
-    __tablename__ = 'questionVictimRace'
-    questionVictimRaceID = Column(Integer, primary_key=True)
-    victimRace = Column(String(30))
+    __tablename__ = 'question_victim_race'
+    question_victim_race_id = Column(Integer, primary_key=True)
+    victim_race = Column(String(30))
 
 # answers
 class ReportData(Base):
-    __tablename__ = 'reportData'
-    reportDataID = Column(Integer, primary_key=True)
-    reportEmail = Column(String(255), nullable=False, unique=False)
-    reportedBy = relationship('ReportedBy', backref='reported_by')
-    victimInformation = relationship('VictimInformation', backref='victim_info')
-    publicRelations = relationship('PublicRelations', backref='public_relations')
-    policeInformation = relationship('PoliceInformation', backref='police_info')
+    __tablename__ = 'report_data'
+    report_data_id = Column(Integer, primary_key=True)
+    report_email = Column(String(255), nullable=False, unique=False)
+    reported_by = relationship('ReportedBy', backref='reported_by_')
+    victim_information = relationship('VictimInformation', backref='victim_info_')
+    public_relations = relationship('PublicRelations', backref='public_relations_')
+    police_information = relationship('PoliceInformation', backref='police_info_')
 
 class ReportedBy(Base):
-    __tablename__ = 'reportedBy'
-    reportedByID = Column(Integer, primary_key=True, autoincrement=True)
-    confirmEmail = Column(String(255), nullable=False, unique=False)
-    reportType = relationship('ReportType', backref='report_type')
-    reportDate = relationship('ReportDate', backref='report_date')
-    reportDataID = Column(Integer, ForeignKey('reportData.reportDataID'))
+    __tablename__ = 'reported_by'
+    reported_by_id = Column(Integer, primary_key=True, autoincrement=True)
+    confirm_email = Column(String(255), nullable=False, unique=False)
+    report_type = relationship('ReportType', backref='report_type_')
+    report_date = relationship('ReportDate', backref='report_date_')
+    report_data_id = Column(Integer, ForeignKey('report_data.report_data_id'))
 
 class VictimInformation(Base):
-    __tablename__ = 'victimInformation'
-    victimInformationID = Column(Integer, primary_key=True, autoincrement=True)
-    numberOfVictims = Column(String(8), nullable=False)
-    victimAge = Column(String(8), nullable=False)
-    victimGender = Column(String(25), nullable=False)
-    victimRace = Column(String(10), nullable=False)
-    reportDataID = Column(Integer, ForeignKey('reportData.reportDataID'))
+    __tablename__ = 'victim_information'
+    victim_information_id = Column(Integer, primary_key=True, autoincrement=True)
+    number_of_victims = Column(String(8), nullable=False)
+    victim_age = Column(String(8), nullable=False)
+    victim_gender = Column(String(25), nullable=False)
+    victim_race = Column(String(10), nullable=False)
+    report_data_id = Column(Integer, ForeignKey('report_data.report_data_id'))
 
 class PublicRelations(Base):
-    __tablename__ = 'publicRelations'
-    publicRelationsID = Column(Integer, primary_key=True, autoincrement=True)
-    searchReason = Column(String(55), nullable=False)
-    searchType = Column(String(10), nullable=False)
-    additionalNotes = Column(Text, nullable=True)
-    incidentAddress = relationship('IncidentAddress', backref='incident_address')
-    reportDataID = Column(Integer, ForeignKey('reportData.reportDataID'))
+    __tablename__ = 'public_relations'
+    public_relations_id = Column(Integer, primary_key=True, autoincrement=True)
+    search_reason = Column(String(55), nullable=False)
+    search_type = Column(String(10), nullable=False)
+    additional_notes = Column(Text, nullable=True)
+    incident_address = relationship('IncidentAddress', backref='incident_address_')
+    report_data_id = Column(Integer, ForeignKey('report_data.report_data_id'))
 
 class PoliceInformation(Base):
-    __tablename__ = 'policeInformation'
-    policeInformationID = Column(Integer, primary_key=True, autoincrement=True)
-    numberOfPolice = Column(String(8), nullable=False)
-    obtainPoliceInfo = Column(Integer, nullable=False) # 0 = False, 1 = True
-    officerInformation = relationship('OfficerInformation', backref='officer_information')
-    reportDataID = Column(Integer, ForeignKey('reportData.reportDataID'))
+    __tablename__ = 'police_information'
+    police_information_id = Column(Integer, primary_key=True, autoincrement=True)
+    number_of_police = Column(String(8), nullable=False)
+    obtain_police_info = Column(Integer, nullable=False) # 0 = False, 1 = True
+    officer_information = relationship('OfficerInformation', backref='officer_information_')
+    report_data_id = Column(Integer, ForeignKey('report_data.report_data_id'))
 
 class ReportType(Base):
-    __tablename__ = 'reportType'
-    reportTypeID = Column(Integer, primary_key=True, autoincrement=True)
-    reportType = Column(String(9), nullable=False)
-    reportedByID = Column(Integer, ForeignKey('reportedBy.reportedByID'))
+    __tablename__ = 'report_type'
+    report_type_id = Column(Integer, primary_key=True, autoincrement=True)
+    report_type = Column(String(9), nullable=False)
+    reported_by_id = Column(Integer, ForeignKey('reported_by.reported_by_id'))
 
 class ReportDate(Base):
-    __tablename__ = 'reportDate'
-    reportDateID = Column(Integer, primary_key=True, autoincrement=True)
-    reportDate = Column(String(20), nullable=False)
-    formattedDay = Column(String(4), nullable=False)
-    formattedWeekday = Column(String(9), nullable=False)
-    formattedMonth = Column(String(9), nullable=False)
-    formattedYear = Column(String(4), nullable=False)
-    formattedTime = Column(String(8), nullable=False)
-    reportedByID = Column(Integer, ForeignKey('reportedBy.reportedByID'))
+    __tablename__ = 'report_date'
+    report_date_id = Column(Integer, primary_key=True, autoincrement=True)
+    report_date = Column(String(20), nullable=False)
+    formatted_day = Column(String(4), nullable=False)
+    formatted_weekday = Column(String(9), nullable=False)
+    formatted_month = Column(String(9), nullable=False)
+    formatted_year = Column(String(4), nullable=False)
+    formatted_time = Column(String(8), nullable=False)
+    reported_by_id = Column(Integer, ForeignKey('reported_by.reported_by_id'))
 
 class IncidentAddress(Base):
-    __tablename__ = 'incidentAddress'
-    incidentAddressID = Column(Integer, primary_key=True, autoincrement=True)
+    __tablename__ = 'incident_address'
+    incident_address_id = Column(Integer, primary_key=True, autoincrement=True)
     addressType = Column(String(16), nullable=False)  # automaticAddress or manualAddress
-    streetName = Column(String(50), nullable=False)
-    townOrCity = Column(String(50), nullable=False)
+    street_name = Column(String(50), nullable=False)
+    town_or_city = Column(String(50), nullable=False)
     country = Column(String(50), default='UK')
-    mapCoordinates = relationship('MapCoordinates', backref='map_coordinates')
-    publicRelationsID = Column(Integer, ForeignKey('publicRelations.publicRelationsID'))
-
-
+    map_coordinates = relationship('MapCoordinates', backref='map_coordinates_')
+    public_relations_id = Column(Integer, ForeignKey('public_relations.public_relations_id'))
 
 class OfficerInformation(Base):
-    __tablename__ = 'officerInformation'
-    officerInformationID = Column(Integer, primary_key=True, autoincrement=True)
-    badgeNumber =  Column(String(10), nullable=True)
-    officerName =  Column(String(50), nullable=True)
-    policeStation =  Column(String(50), nullable=True)
-    policeInformationID = Column(Integer, ForeignKey('policeInformation.policeInformationID'))
+    __tablename__ = 'officer_information'
+    officer_information_id = Column(Integer, primary_key=True, autoincrement=True)
+    badge_number =  Column(String(10), nullable=True)
+    officer_name =  Column(String(50), nullable=True)
+    police_station =  Column(String(50), nullable=True)
+    police_information_id = Column(Integer, ForeignKey('police_information.police_information_id'))
     
 class MapCoordinates(Base):
-    __tablename__ = 'mapCoordinates'
-    mapCoordinatesID = Column(Integer, primary_key=True, autoincrement=True)
+    __tablename__ = 'map_coordinates'
+    map_coordinates_id = Column(Integer, primary_key=True, autoincrement=True)
     longitude = Column(Float, nullable=False)
     latitude = Column(Float, nullable=False)
-    incidentAddressID = Column(Integer, ForeignKey('incidentAddress.incidentAddressID'))
+    incident_address_id = Column(Integer, ForeignKey('incident_address.incident_address_id'))
