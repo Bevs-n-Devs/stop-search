0208e9cb534b9d5b692299c56c04e39615a2d2e2
PythonAkoto <yawakoto.python@example.com>
2024-08-21 19:35:36 +0100

    logic to save datra from database into MySQL DB

--- stopSearch/main.py
+++ stopSearch/main.py
@@ -1,11 +1,13 @@
 from stopSearch import app
-from stopSearch.stopSearch_api import index_route, questions_route, submit_report
-from stopSearch.stopSearch_endpoint import report_endpoint
+from stopSearch.stopSearch_api import index_route, questions_route, submit_report, search_engine
+from stopSearch.stopSearch_endpoint import report_endpoint, map_endpoint
 
 # BE
 app.route("/")(index_route)
 app.route("/questions")(questions_route)
 app.route("/submit_report")(submit_report)
+app.route("/search")(search_engine)
 
 # FE
 app.route("/report")(report_endpoint)
+app.route("/map")(map_endpoint)
